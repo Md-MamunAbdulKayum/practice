681e35ef194b89e1e83aa443da2e59c453c3e51f
Md-MamunAbdulKayum <souravcuetcse2010@gmail.com>
2017-04-28 01:35:48 -0700

    Test project uploaded

---
+++ Test/src/test/Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Kayum-603
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = null;
+        if (s == null) {
+            System.out.println("null");
+        } else {
+            System.out.println("not null");
+        }
+    }
+    
+}
